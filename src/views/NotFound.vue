219f8b7e32ceb407f345dbd1fe3c46262db6e9df
Justin Robb <reptarsrage@gmail.com>
2019-04-20 17:11:23 -0700

    Adding not found page

---
+++ src/views/NotFound.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-container fill-height>
+    <v-layout align-start justify-start>
+      <v-flex>
+        <h4 class="display-1">404</h4>
+        <h1 class="display-4">Not Found</h1>
+        <v-btn to="/">
+          <v-icon left>home</v-icon>
+          <span>Go Home</span>
+        </v-btn>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
