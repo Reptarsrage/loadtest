cbdeb3ee4115697a361b7ca183c4e086310dc945
Justin Robb <reptarsrage@gmail.com>
2019-05-11 13:48:55 -0700

    Fixed test

--- test/Main.spec.js
+++ test/Main.spec.js
@@ -1,9 +1,9 @@
 import { mount } from '@vue/test-utils'
-import Logo from '@/components/Logo.vue'
+import Main from '@/components/Main.vue'
 
-describe('Logo', () => {
+describe('Main', () => {
   test('is a Vue instance', () => {
-    const wrapper = mount(Logo)
+    const wrapper = mount(Main)
     expect(wrapper.isVueInstance()).toBeTruthy()
   })
 })
